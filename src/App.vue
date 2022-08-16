d64df18c9be0721999e74447deac84ac1dbe432e
大雄吖 <2981451773@qq.com>
2022-08-16 20:49:37 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>hahhahhhh</h1>
+  </div>
+</template>
+
+<script>
+import {roleList} from 'api/roleManagement/roleList'
+  export default {
+  data() {
+    return {
+    };
+  },
+  created() {
+    this.getRoleList()
+  },
+  methods: {
+    async getRoleList(){
+      const res = await roleList()
+      console.log(res)
+    },
+  },
+}
+</script>
+
+<style lang='less' scoped>
+</style>
---
+++ src/api/roleManagement/roleUpdate.js
@@ -0,0 +1,10 @@
+// import http from 'utils/request.js' //导入request.js的axios配置
+
+// //请求用户列表
+// export const roleUpdate = (params) => {
+//     return http ({
+//         method:"get",
+//         url:'/role',
+//         params:params
+//     })
+// }
